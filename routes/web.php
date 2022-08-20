685cb8af688832f15bc73b7170d2d986c73002cc
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-08-20 18:06:38 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\YourController;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/url/{parameter}', function ($id) {
+    $data = Crypt::decrypt($id);
+    return $data;
+});
